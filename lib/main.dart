098922b55e9e51f14dbe44d543f44a6c6a0792bf
Alex <tweneboaosei@gmail.com>
2022-07-25 11:57:32 +0000

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:projectwork/signup.dart';
 import 'package:projectwork/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
